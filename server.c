55df1b1f8d465e9ed031457208ff5e8161d68ebd
sedoy-jango-2 <aa18.04.99@gmail.com>
2018-12-18 00:20:44 +0300

    Add files via upload

---
+++ server.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+
+#define SHM_ID  2002
+#define SEM_ID	2001
+#define PERMS	0666
+
+typedef struct
+{
+	int num;
+	int smoke;
+	int buy;
+}ticket;
+
+void sys_err (char *msg)
+{
+	puts (msg);
+	exit (1);
+}
+
+int main(int argc, char **argv)
+{
+	int shmid, semid;
+	int n = 7, i;
+	ticket *temp, *temp_local;
+	
+	if ((semid = semget (SEM_ID, 1, PERMS | IPC_CREAT)) < 0)
+		sys_err ("server: can not create semaphore");
+	
+	if ((shmid = shmget (SHM_ID, sizeof(temp), PERMS|IPC_CREAT)) < 0)
+		sys_err ("client: can not get shared memory segment");
+
+	if ((temp = (ticket *) shmat(shmid, 0, 0)) == NULL)
+		sys_err ("client: shared memory attach error");
+	
+	semctl (semid, 0, SETVAL, 1);
+	
+	for (i = 0; i < n; i++)
+	{
+		temp[i].num = temp_local[i].num = i;
+		temp[i].buy = temp_local[i].buy = 0;
+		if (temp[i].num % 2)
+			temp[i].smoke = temp_local[i].smoke = 0;
+		else
+			temp[i].smoke = temp_local[i].smoke = 1;
+		printf("%d, %d, %d\n", temp[i].num, temp[i].smoke, temp[i].buy)
+	}
+	
+	semctl (semid, 0, SETVAL, 0);
+        
+	loop:
+		if !(semctl (semid, 0, GETVAL, 0))
+		{
+			semctl (semid, 0, SETVAL, 1);
+			for (i = 0; i < n; i++)
+			{
+				if (temp[i].buy != temp_local[i].buy)
+				{
+					printf("Bought ticket # %d\n", i);
+					temp_local[i].buy = temp[i].buy;
+				}
+			}
+			semctl (semid, 0, SETVAL, 0);
+		}
+	goto loop;
+	
+	exit (0);
+}
