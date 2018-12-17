9ca3afd42d7a9c281151752fd7ac6688b5bbdc55
sedoy-jango-2 <aa18.04.99@gmail.com>
2018-12-17 21:34:33 +0300

    Update server.c

--- server.c
+++ server.c
@@ -6,34 +6,36 @@
 #include <sys/sem.h>
 #include <sys/shm.h>
 
+#define SHM_ID	2002
+
 typedef struct
 {
-  int num;
-  int smoke;
-  int buy;
+	int num;
+	int smoke;
+	int buy;
 } ticket;
 
 void sys_err (char *msg)
 {
-  puts (msg);
-  exit (1);
+	puts (msg);
+	exit (1);
 }
 
-int main ()
+int main(int argc, char **argv)
 {
-  int shmid; 
-  ticket *temp;           
+	int shmid; 
+	ticket *temp;           
 
-  if ((shmid = shmget (SHM_ID, sizeof (temp), 0)) < 0)
-    sys_err ("client: can not get shared memory segment");
+	if ((shmid = shmget (SHM_ID, sizeof(temp), 0666|IPC_CREAT)) < 0)
+		sys_err ("client: can not get shared memory segment");
 
-  if ((temp = (ticket *) shmat (shmid, 0, 0)) == NULL)
-    sys_err ("client: shared memory attach error");
+	if ((temp = (ticket *) shmat(shmid, 0, 0)) == NULL)
+		sys_err ("client: shared memory attach error");
 
-  temp->num = 0;
-  temp->smoke = 0;
-  temp->buy = 0;
+	temp->num = 0;
+	temp->smoke = 0;
+	temp->buy = 0;
   
-  shmdt (temp);
-  exit (0);
+	shmdt (temp);
+	exit (0);
 }
